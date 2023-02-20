8e5dea027dd9e4adbef72cc8f9fbc62d60710c31
Manny <emaldonado1402@gmail.com>
2023-02-19 22:13:57 -0500

    Contact Page module css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import styles from "./HomePage.module.css"
+
 const HomePage = () => {
   return(
   <>
-    <h1> hello world </h1>
+    <div className={styles.introImg}>
+      <img src="images/DND_Purple_1.jpg" alt="Manny" />
+    </div>
+    <div>
+      <h1 className={styles.introHead}> My name is Emmanuel Maldonado aka Manu</h1>
+      <p className={styles.intro}> I'm a Software Engineer I love to problem solve and use the trial and error method.</p>
+    </div>
   </>
   )
 }
