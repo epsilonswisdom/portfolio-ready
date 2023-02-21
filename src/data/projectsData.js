26054651c2cc9094f14b3c1592a6c67b104f5e7b
Manny <emaldonado1402@gmail.com>
2023-02-21 14:11:44 -0500

    Create a projects page and import the data from project data

---
+++ src/data/projectsData.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    title: 'Connect-Four',
+    description: 'A personalized Connect Four game. I expressed everything I love personally in this game',
+    image: 'https://github.com/epsilonswisdom/mannys-connectfour/raw/main/Assets/Connect%204%20Snapshot.png',
+    repositoryLink: 'https://github.com/epsilonswisdom/mannys-connectfour.git',
+    deploymentLink: 'https://mannysconnectfour.netlify.app/'
+  },
+
+  {
+    title: 'GameForge',
+    description: 'I made this blog site as an outlet for my love of gaming and with those whom share the same feeling',
+    image: 'https://github.com/epsilonswisdom/game-forgev1/raw/main/assets/Screenshot_20230120_110757.png',
+    repositoryLink: 'https://github.com/epsilonswisdom/game-forgev1.git',
+    deploymentLink: 'https://gamer-forge.fly.dev/'
+  }
+]
